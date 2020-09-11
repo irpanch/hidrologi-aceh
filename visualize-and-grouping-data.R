9061cc3c8a8d7995a374ea1d1f4320269f03a5f2
irpanch <irpanch@gmail.com>
2020-09-11 16:49:35 +0700

    update minor

--- visualize-and-grouping-data.R
+++ visualize-and-grouping-data.R
@@ -1,14 +1,17 @@
 # visualize and grouping data
 
 ## import data
-library(readr)
-data <- read_csv("data_hujan_gabung.csv") 
 
+
+library(readr)
+## ---- data-hujan
+data <- read_csv("data_hujan_gabung.csv") 
 data <- data[,-1] # hilangkan kolom nomor
 
 # data hujan harian setiap pos hujan
 library(ggplot2)
 
+## ---- data-pisah
 plot_pisah <- ggplot(data,aes(Tanggal, Curah_Hujan,col=Nama_Stasiun)) +
   geom_line() + 
   labs(title="Data Curah Hujan Pada Masing-masing Stasiun",
@@ -18,9 +21,8 @@ plot_pisah <- ggplot(data,aes(Tanggal, Curah_Hujan,col=Nama_Stasiun)) +
   theme_update(plot.title=element_text(hjust=0.5))+
   theme_update(plot.subtitle=element_text(hjust=0.5))+
   theme_update(axis.title.y=element_text(angle=90)) 
-plot_pisah
 
-
+## ---- plot-gabung
 plot_gabung <- ggplot(data,aes(Tanggal, Curah_Hujan,col=Nama_Stasiun)) +
   geom_line() + 
   labs(title="Data Curah Hujan Pada Masing-masing Stasiun",
@@ -30,3 +32,16 @@ plot_gabung <- ggplot(data,aes(Tanggal, Curah_Hujan,col=Nama_Stasiun)) +
   theme_update(plot.subtitle=element_text(hjust=0.5))+
   theme_update(axis.title.y=element_text(angle=90))
 plot_gabung
+
+## ---- plot-pisah
+ggplot(data,aes(Tanggal, Curah_Hujan,col=Nama_Stasiun)) +
+  geom_line() + 
+  labs(title="Data Curah Hujan Pada Masing-masing Stasiun",
+       y="Curah Hujan (mm)",
+       x="Waktu") + 
+  facet_wrap(~Nama_Stasiun)+
+  theme_update(plot.title=element_text(hjust=0.5))+
+  theme_update(plot.subtitle=element_text(hjust=0.5))+
+  theme_update(axis.title.y=element_text(angle=90)) 
+
+
