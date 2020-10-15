e1b9485470f713815ee0e30c0d03b36f3b74442f
irpanch <irpanch@gmail.com>
2020-10-15 22:07:37 +0700

    adding pivot table

--- visualize-and-grouping-data.R
+++ visualize-and-grouping-data.R
@@ -46,3 +46,41 @@ ggplot(data,aes(Tanggal, Curah_Hujan,col=Nama_Stasiun)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
+# tambah kolom hari, bulan, dan tahun.
+library(lubridate)
+data$DOY <- as.numeric(yday(data$Tanggal))
+data$YEAR <- as.numeric(format(data$Tanggal,"%Y"))
+data$MONTH <- as.numeric(format(data$Tanggal,"%m"))
+data$Bulan <- month.abb[data$MONTH]
+data <- data[,1:7]
+
+View(data)
+
+# Jumlah curah hujan bulanan
+
+nama_sta1 <- "Pegasing"
+
+# max curah hujan harian
+
+## ---- CurahHujan_sta1
+library(pivottabler)
+data_sta1 <- subset(data,data$Nama_Stasiun == nama_sta1)
+pivot_sta1 <- PivotTable$new()
+pivot_sta1$addData(data_sta1)
+pivot_sta1$addColumnDataGroups("Bulan")
+pivot_sta1$sortColumnDataGroups(levelNumber=1,orderBy="customByValue",
+                                sortOrder="asc",
+                                customOrder=c("Jan","Feb","Mar",
+                                              "Apr","May","Jun",
+                                              "Jul","Aug","Sep",
+                                              "Oct", "Nov","Dec"))
+pivot_sta1$addRowDataGroups("YEAR")
+pivot_sta1$defineCalculation(calculationName="Max",summariseExpression="max(Curah_Hujan)")
+# cells <- pivot_sta1$findCells(minValue=100,totals="exclude")
+# pivot_sta1$setStyling(cells=cells, declaration=list("background-color"="#FFC7CE", "color"="#9C0006"))
+# pivot_sta1$renderPivot()
+pivot_sta1$evaluatePivot()
+sta1_df <- pivot_sta1$asDataFrame()
+sta1_df
+
+
